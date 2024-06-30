1d63f4c49c44bd8cc053d9fe2ee8d798f86b59a7
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-06-30 17:37:47 +0700

    Death Event

---
+++ src/main/java/com/aspectxlol/barudakmc/listener/onDeathListener.java
@@ -0,0 +1,29 @@
+package com.aspectxlol.barudakmc.listener;
+
+import com.aspectxlol.barudakmc.DiscordWebhook;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.io.IOException;
+
+public class onDeathListener implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        DiscordWebhook discordWebhook = new DiscordWebhook("https://discord.com/api/webhooks/1256894119876362261/3wtZqGIU7w69j-7vgzrP_o-jg724LeGgdHMSYaq6R74zujtBlKprg4G6E4T5kifguNH3");
+        discordWebhook.setUsername(event.getEntity().getDisplayName());
+        discordWebhook.setAvatarUrl("https://api.tydiumcraft.net/v1/players/skin?uuid=" + event.getEntity().getUniqueId() + "&type=avatar");
+
+        DiscordWebhook.EmbedObject JoinEmbed = new DiscordWebhook.EmbedObject();
+        JoinEmbed.setTitle(event.getDeathMessage());
+
+        try {
+            discordWebhook.execute();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
