7b91670fe337e215b066014bbe2e6d9fa6b856eb
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-07-07 14:18:46 +0700

    drop heads on death

--- src/main/java/com/aspectxlol/barudakmc/listener/onDeathListener.java
+++ src/main/java/com/aspectxlol/barudakmc/listener/onDeathListener.java
@@ -2,11 +2,21 @@ package com.aspectxlol.barudakmc.listener;
 
 import com.aspectxlol.barudakmc.BarudakMC;
 import com.aspectxlol.barudakmc.DiscordWebhook;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class onDeathListener implements Listener {
 
@@ -33,6 +43,33 @@ public class onDeathListener implements Listener {
             throw new RuntimeException(e);
         }
 
+
+        assert event.getEntity().getPlayer() != null;
+
+        ItemStack head = getHead(event.getEntity().getPlayer());
+        ItemMeta headMeta = head.getItemMeta();
+        assert headMeta != null;
+        headMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&7" + event.getEntity().getPlayer().getName()));
+
+        Entity e = event.getEntity().getKiller();
+        if(e != null) {
+            List<String> Lore = new ArrayList<>();
+            Lore.add(ChatColor.translateAlternateColorCodes('&', "Was Killed By &6" + event.getEntity().getKiller().getDisplayName()));
+            headMeta.setLore(Lore);
+        }
+        head.setItemMeta(headMeta);
+
+        event.getDrops().add(head);
+    }
+
+    private ItemStack getHead(Player player) {
+        ItemStack item = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta skull = (SkullMeta) item.getItemMeta();
+        assert skull != null;
+        skull.setDisplayName(player.getName());
+        skull.setOwningPlayer(Bukkit.getOfflinePlayer(player.getUniqueId()));
+        item.setItemMeta(skull);
+        return item;
     }
 
 }
